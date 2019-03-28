2e5db9c8bb2ca4c3c2bc880a93706d9d2033fe36
Arthur Mesquita Pickcius <arthur.pickcius@gmail.com>
2019-03-28 09:54:31 -0300

    viewport signal in progress

--- Main.py
+++ Main.py
@@ -2,12 +2,17 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+import Viewport
+
 class Handler:
-    def onMainWindowDestroy(self, *args):
+	def onMainWindowDestroy(self, *args):
         Gtk.main_quit()
 
-    def onListRightclicked(self, button, user_data):
+	def onListRightclicked(self, button, user_data):
         print("Hello World!")
+
+	def onDraw(self,widget,event):
+		Viewport.draw(widget,event)
 
 builder = Gtk.Builder()
 builder.add_from_file("ui.glade")
@@ -15,5 +20,8 @@ builder.connect_signals(Handler())
 
 window = builder.get_object("window_main")
 window.show_all()
+
+viewp = builder.get_object("viewport")
+# viewp.gdjskafhgdksafkjdfskljgshgf
 
 Gtk.main()
