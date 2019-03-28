26237b57b2d249baebf4f50a56f55ac44e0e8d73
A.M.Pixels <9497046+ArthurPixels@users.noreply.github.com>
2019-03-28 09:17:53 -0300

    Hello world on right click

--- Main.py
+++ Main.py
@@ -6,14 +6,14 @@ class Handler:
     def onMainWindowDestroy(self, *args):
         Gtk.main_quit()
 
-    def onListRightclicked(self, button):
+    def onListRightclicked(self, button, user_data):
         print("Hello World!")
 
 builder = Gtk.Builder()
 builder.add_from_file("ui.glade")
 builder.connect_signals(Handler())
 
-window = builder.get_object("window1")
+window = builder.get_object("window_main")
 window.show_all()
 
 Gtk.main()
