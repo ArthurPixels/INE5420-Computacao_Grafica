73f3e1db8409a1b9a7a1d09d0df26704e46a72b5
Arthur Mesquita Pickcius <arthur.pickcius@gmail.com>
2019-03-30 19:32:57 -0300

    create object hello world

--- main.py
+++ main.py
@@ -5,30 +5,43 @@ from gi.repository import Gtk
 # import Viewport
 
 class Handler:
+    def __init__(self,builder):
+        self.builder = builder
+
     def onMainWindowDestroy(self, *args):
         Gtk.main_quit()
 
     def onListRightclicked(self, button, user_data):
-        print("Hello World!")
+        # print("Hello World!")
+        dialog_add_object = self.builder.get_object("dialog_add_object")
+        dialog_add_object.show_all()
 
     def onDraw(self,widget,event):
-        Viewport.draw(widget,event)
-
-    def bt_create_object_clicked_cb():
         pass
+        # self.viewp.draw(widget,event)
 
-class WindowBuilder():
+    def bt_create_object_clicked_cb(self, button):
+        lb_point_x = self.builder.get_object("lb_point_x")
+        lb_point_y = self.builder.get_object("lb_point_y")
+        try:
+            x = int(lb_point_x.get_text())
+            y = int(lb_point_y.get_text())
+            print(x)
+            print(y)
+        except:
+            print("Invalid value")
+
+class WindowBuilder:
     def run(self):
         builder = Gtk.Builder()
         builder.add_from_file("ui.glade")
-        builder.add_from_file("include_object.glade")
-        builder.connect_signals(Handler())
+        builder.add_from_file("add_object.glade")
+        builder.connect_signals(Handler(builder))
 
         window = builder.get_object("window_main")
         window.show_all()
 
-        viewp = builder.get_object("viewport")
-        # viewp.gdjskafhgdksafkjdfskljgshgf
+        # viewp = builder.get_object("viewport")
 
         Gtk.main()
 
