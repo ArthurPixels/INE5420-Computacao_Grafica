26eaf5668a1ee6907632b3c94b787b594d071626
Arthur Mesquita Pickcius <arthur.pickcius@gmail.com>
2019-03-31 14:10:57 -0300

    fight against treeview

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+from object import Object
 
 # import Viewport
 
+class DisplayFile:
+    def __init__(self, dr_area):
+        self.objects = []
+        self.viewport = dr_area
+
+    def append(self,obj):
+        self.objects.append(obj)
+
+    def update(self):
+        for obj in self.objects:
+            # self.ui_obj_list.AddItem(obj.name)
+            pass
+
 class Handler:
-    def __init__(self,builder):
+    def __init__(self,df,builder):
         self.builder = builder
+        self.display_file = df
+        self.ui_obj_list = builder.get_object("obj_list")
+        self.store = Gtk.ListStore(str)
+        self.ui_obj_list.set_model(self.store)
+        print("Handler init ok")
 
     def onMainWindowDestroy(self, *args):
         Gtk.main_quit()
 
-    def onListRightclicked(self, button, user_data):
-        # print("Hello World!")
+    def obj_list_clicked_cb(self, button, user_data):
+        self.builder.add_from_file("add_object.glade")
+        self.builder.connect_signals(Handler(self.display_file,self.builder))
         dialog_add_object = self.builder.get_object("dialog_add_object")
         dialog_add_object.show_all()
 
@@ -21,22 +41,28 @@ class Handler:
         # self.viewp.draw(widget,event)
 
     def bt_create_object_clicked_cb(self, button):
-        lb_point_x = self.builder.get_object("lb_point_x")
-        lb_point_y = self.builder.get_object("lb_point_y")
+        entry_obj_name = self.builder.get_object("entry_obj_name")
+        entry_point_x = self.builder.get_object("entry_point_x")
+        entry_point_y = self.builder.get_object("entry_point_y")
         try:
-            x = int(lb_point_x.get_text())
-            y = int(lb_point_y.get_text())
-            print(x)
-            print(y)
+            x = float(entry_point_x.get_text())
+            y = float(entry_point_y.get_text())
         except:
-            print("Invalid value")
+            print("Error: Invalid value")
+        
+        obj = Object(entry_obj_name.get_text(),x,y)
+        self.display_file.append(obj)
+        print(x)
+        print(y)
+        self.store.append([obj.name])
+        self.display_file.update()
 
 class WindowBuilder:
     def run(self):
         builder = Gtk.Builder()
         builder.add_from_file("ui.glade")
-        builder.add_from_file("add_object.glade")
-        builder.connect_signals(Handler(builder))
+        df = DisplayFile(builder.get_object("viewport"))
+        builder.connect_signals(Handler(df,builder))
 
         window = builder.get_object("window_main")
         window.show_all()
---
+++ object.py
@@ -0,0 +1,10 @@
+class Object:
+    def __init__(self,name,x0,y0,z0=0):
+        self.name = name
+        self.points = [(x0,y0,z0)]
+
+# class Point:
+#     def __init__(self, x, y, z=0):
+#         self.x = x
+#         self.y = y
+#         self.z = z
